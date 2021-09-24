3fb63609b62f005eddf4b0a318912a8e6a02794b
soumyajit4419 <ashish454570@gmail.com>
2021-09-25 01:58:31 +0530

    feature(Toc v1.0)

--- Components/Toc.js
+++ Components/Toc.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 function Toc({ headings }) {
+  // useEffect(() => {
+  //   const title = document.querySelectorAll("h2, h3");
+  //   console.log(title);
+  // }, []);
+
+  const [active, setActive] = useState("");
+  console.log(active);
+
   return (
-    <nav className="sticky top-36 overflow-auto">
-      <h3 className="text-2xl uppercase text-gray-800 dark:text-gray-200">
-        Table Of Contents
-      </h3>
+    <nav className="sticky top-32 overflow-auto toc-inner">
       <ul>
         {headings.map((heading, index) => (
           <li
@@ -14,6 +19,10 @@ function Toc({ headings }) {
             className="mt-4 text-lg text-gray-700 dark:text-gray-400"
             style={{
               paddingLeft: heading.level === 3 ? "1rem" : "",
+              color: heading.id === active ? "#6366f1" : "",
+            }}
+            onClick={(e) => {
+              setActive(heading.id);
             }}
           >
             <Link href={`#${heading.id}`}>{heading.text}</Link>
